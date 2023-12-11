6f25fefdbc277221b452edfa70393f0f4ef690b6
Bastien <bastien.b200405@gmail.com>
2023-12-11 11:49:46 +0100

    Add files

---
+++ extract.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("test");
+    return 0;
+}
+
+int open(const char *chemin, int typeOpen, mode_t droitsFic) {
+
+
+
+}
